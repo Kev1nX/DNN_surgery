51fc08aba70ecae8f6aefc519ebd9dbeb9ea7092
Kev1nX <kevin0706x@gmail.com>
2025-06-29 18:59:35 +1000

    Initialized grpc

--- dnn_surgery/dnn_surgery.py
+++
@@ -1,6 +0,0 @@
-from dataset.importer import CIFAR10_dataset
-from networks.cnn import Net
-from utils.inference_size_estimator import (
-    nn_layer_parameter_sizes,
-    calculate_total_parameter_size,
-)
--- server.py
+++ server.py
@@ -1,39 +1,133 @@
-# server.py
-import torch
-import torch.distributed.rpc as rpc
-from my_function import add_tensors
+import json
+import time
+import math
+import grpc
+import logging
+import gRPC.protobuf.route_guide_pb2 as route_guide_pb2
+import gRPC.protobuf.route_guide_pb2_grpc as route_guide_pb2_grpc
+from concurrent import futures
 
 
-def run_server(name, worker_addr, world_size):
+
+
+def read_route_guide_database():
+    """Reads the route guide database.
+
+    Returns:
+      The full contents of the route guide database as a sequence of
+        route_guide_pb2.Features.
     """
-    The function to run on the server.
+    feature_list = []
+    with open("route_guide_db.json") as route_guide_db_file:
+        for item in json.load(route_guide_db_file):
+            feature = route_guide_pb2.Feature(
+                name=item["name"],
+                location=route_guide_pb2.Point(
+                    latitude=item["location"]["latitude"],
+                    longitude=item["location"]["longitude"],
+                ),
+            )
+            feature_list.append(feature)
+    return feature_list
 
-    :param name: The name of the server
-    :param worker_addr: The address of the worker node
-    :param world_size: The total number of nodes
-    """
-    options = {
-        "rpc_backend_options": rpc.TensorPipeRpcBackendOptions(
-            init_method=f"tcp://{worker_addr}"
+def get_feature(feature_db, point):
+    """Returns Feature at given location or None."""
+    for feature in feature_db:
+        if feature.location == point:
+            return feature
+    return None
+
+def get_distance(start, end):
+    """Distance between two points."""
+    coord_factor = 10000000.0
+    lat_1 = start.latitude / coord_factor
+    lat_2 = end.latitude / coord_factor
+    lon_1 = start.longitude / coord_factor
+    lon_2 = end.longitude / coord_factor
+    lat_rad_1 = math.radians(lat_1)
+    lat_rad_2 = math.radians(lat_2)
+    delta_lat_rad = math.radians(lat_2 - lat_1)
+    delta_lon_rad = math.radians(lon_2 - lon_1)
+
+    # Formula is based on http://mathforum.org/library/drmath/view/51879.html
+    a = pow(math.sin(delta_lat_rad / 2), 2) + (
+        math.cos(lat_rad_1)
+        * math.cos(lat_rad_2)
+        * pow(math.sin(delta_lon_rad / 2), 2)
+    )
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    R = 6371000
+    # metres
+    return R * c
+
+class RouteGuideServicer(route_guide_pb2_grpc.RouteGuideServicer):
+    """Provides methods that implement functionality of route guide server."""
+
+    def __init__(self):
+        self.db = read_route_guide_database()
+
+    def GetFeature(self, request, context):
+        feature = get_feature(self.db, request)
+        if feature is None:
+            return route_guide_pb2.Feature(name="", location=request)
+        else:
+            return feature
+
+    def ListFeatures(self, request, context):
+        left = min(request.lo.longitude, request.hi.longitude)
+        right = max(request.lo.longitude, request.hi.longitude)
+        top = max(request.lo.latitude, request.hi.latitude)
+        bottom = min(request.lo.latitude, request.hi.latitude)
+        for feature in self.db:
+            if (
+                feature.location.longitude >= left
+                and feature.location.longitude <= right
+                and feature.location.latitude >= bottom
+                and feature.location.latitude <= top
+            ):
+                yield feature
+
+    def RecordRoute(self, request_iterator, context):
+        point_count = 0
+        feature_count = 0
+        distance = 0.0
+        prev_point = None
+
+        start_time = time.time()
+        for point in request_iterator:
+            point_count += 1
+            if get_feature(self.db, point):
+                feature_count += 1
+            if prev_point:
+                distance += get_distance(prev_point, point)
+            prev_point = point
+
+        elapsed_time = time.time() - start_time
+        return route_guide_pb2.RouteSummary(
+            point_count=point_count,
+            feature_count=feature_count,
+            distance=int(distance),
+            elapsed_time=int(elapsed_time),
         )
-    }
 
-    # Initialize the RPC framework and set the current worker name, rank and the total number of workers
-    rpc.init_rpc(name, rank=0, world_size=world_size, **options)
-
-    # Create two tensors
-    x = torch.ones(3, 3)
-    y = torch.ones(3, 3)
-
-    # Call the function on the worker
-    result = rpc.rpc_sync("worker", add_tensors, args=(x, y))
-
-    # Print the result
-    print(result)
-
-    # Shutdown the RPC framework
-    rpc.shutdown()
+    def RouteChat(self, request_iterator, context):
+        prev_notes = []
+        for new_note in request_iterator:
+            for prev_note in prev_notes:
+                if prev_note.location == new_note.location:
+                    yield prev_note
+            prev_notes.append(new_note)
+            
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
+        RouteGuideServicer(), server
+    )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    run_server("server", "127.0.0.1:8080", 2)
+    logging.basicConfig()
+    serve()
